eb4065e0d8bb674c41e6f816250e07ef409bb2da
bmsant <bmdevelop@outlook.fr>
2024-09-21 03:17:51 +0200

    Theme provider and mode toggle added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Inter } from 'next/font/google'
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: "Bruno Santos",
@@ -24,11 +16,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} bg-background text-foreground`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex flex-col text-center w-1/3 mx-auto gap-4">
-      <p>Welcome to Bruno Santos' website!</p>
-      <Button variant="default">Default Button</Button>
-      <Button variant="secondary">Secondary Button</Button>
-      <Button variant="ghost">Ghost Button</Button>
-      <Button variant="destructive">Destructive Button</Button>
-      <Button variant="outline">Outline Button</Button>
-      <Button variant="link">Link Button</Button>
-    </div>
+    <main>
+      <nav className="flex justify-end p-4">
+        <ModeToggle />
+      </nav>
+      <div className="flex flex-col text-center w-1/3 mx-auto gap-4">
+        <p>Welcome to Bruno Santos' website!</p>
+        <Button variant="default">Default Button</Button>
+        <Button variant="secondary">Secondary Button</Button>
+        <Button variant="ghost">Ghost Button</Button>
+        <Button variant="destructive">Destructive Button</Button>
+        <Button variant="outline">Outline Button</Button>
+        <Button variant="link">Link Button</Button>
+      </div>
+    </main>
   );
 }
