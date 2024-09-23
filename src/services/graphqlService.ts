d712b697a1c5884b7bea9d622d28c478197543e5
bmsant <bmdevelop@outlook.fr>
2024-09-23 13:05:29 +0200

    Fix error with variables

--- src/services/graphqlService.ts
+++ src/services/graphqlService.ts
@@ -2,15 +2,18 @@ import { GET_USER_QUERY } from "@/constants/graphqlQueries";
 
 export async function fetchUserData() {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_ENDPOINT_API}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        query: GET_USER_QUERY,
-      }),
-    });
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_URL_FRONTEND}/${process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT_FRONTEND}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          query: GET_USER_QUERY,
+        }),
+      }
+    );
 
     const result = await response.json();
     if (result.errors) {
